cbf1961591dc6b4a3465d94e9ac9b6a3d26011c7
jo22i <99878853+jo22i@users.noreply.github.com>
2022-03-14 01:57:19 +0300

    changed style to Google

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <string>
 
-int main()
-{
-    std::string name; //Объявление переменной, хранящей имя пользователя
-    std::cout << "Enter your name: "; std::cin >> name; //Ввод имени пользователя
-    std::cout << "Hello world from " << std::name << endl; //Вывод сообщения с именеи=м пользователя
-    return 0;
+int main() {
+  std::string name;  //Объявление переменной, хранящей имя пользователя
+  std::cout << "Enter your name: ";
+  std::cin >> name;  //Ввод имени пользователя
+  std::cout << "Hello world from " << std::name
+            << endl;  //Вывод сообщения с именеи=м пользователя
+  return 0;
 }
