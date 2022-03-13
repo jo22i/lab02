c0865b27ef7e3aabc81509f765900c716e3a4a43
хто я <99878853+jo22i@users.noreply.github.com>
2022-03-14 02:43:51 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -5,6 +5,6 @@ int main()
 {
     std::string name; //Объявление переменной, хранящей имя пользователя
     std::cout << "Enter your name: "; std::cin >> name; //Ввод имени пользователя
-    std::cout << "Hello world from " << std::name << endl; //Вывод сообщения с именеи=м пользователя
+    std::cout << "Hello world from " << std::name << endl; //Вывод сообщения с именем пользователя
     return 0;
 }
