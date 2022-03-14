6fc6e13bb1e2087e17340c04018782c11eca7d79
jo22i <99878853+jo22i@users.noreply.github.com>
2022-03-14 05:09:24 +0300

    some fixes

--- hello_world.cpp
+++ hello_world.cpp
@@ -6,10 +6,6 @@ int main() {
   std::cout << "Enter your name: ";
   std::cin >> name;  //Ввод имени пользователя
   std::cout << "Hello world from " << std::name
-<<<<<<< HEAD
             << endl;  //Вывод сообщения с именем пользователя
-=======
-            << endl;  //Вывод сообщения с именеи=м пользователя
->>>>>>> patch2
   return 0;
 }
