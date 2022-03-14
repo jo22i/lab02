03d6857cc27bd426390c68e44d69a20bbbd61f60
jo22i <99878853+jo22i@users.noreply.github.com>
2022-03-14 04:54:42 +0300

    fixed conflicts between branches

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <string>
 
-int main()
-{
-    std::string name; //Объявление переменной, хранящей имя пользователя
-    std::cout << "Enter your name: "; std::cin >> name; //Ввод имени пользователя
-    std::cout << "Hello world from " << std::name << endl; //Вывод сообщения с именем пользователя
-    return 0;
+int main() {
+  std::string name;  //Объявление переменной, хранящей имя пользователя
+  std::cout << "Enter your name: ";
+  std::cin >> name;  //Ввод имени пользователя
+  std::cout << "Hello world from " << std::name
+            << endl;  //Вывод сообщения с именем пользователя
+  return 0;
 }
